7e9a0c5a656083997d14f5842892972d1f2024ed
JohannaPIRAS <33329743+JohannaPIRAS@users.noreply.github.com>
2017-11-10 08:11:00 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-@@ -0,0 +1,173 @@
 +/*
 + * calculatrice.js 
 +
@@ -112,63 +111,5 @@
 +        $id(id+'_resultat').value=0;
 +        calc_array[id] = new Array('=',1,'0','0',0);
 +        document.getElementById(id+'_resultat').focus();
-+        return true;
-+}
-+function key_detect_calc(id,evt)
-+{
-+        if((evt.keyCode>95) && (evt.keyCode<106))
-+        {
-+                var nbr = evt.keyCode-96;
-+                add_calc(id,nbr);
-+        }
-+        else if((evt.keyCode>47) && (evt.keyCode<58))
-+        {
-+                var nbr = evt.keyCode-48;
-+                add_calc(id,nbr);
-+        }
-+        else if(evt.keyCode==107)
-+        {
-+                f_calc(id,'+');
-+        }
-+        else if(evt.keyCode==109)
-+        {
-+                f_calc(id,'-');
-+        }
-+        else if(evt.keyCode==106)
-+        {
-+                f_calc(id,'*');
-+        }
-+        else if(evt.keyCode==111)
-+        {
-+                f_calc(id,'');
-+        }
-+        else if(evt.keyCode==110)
-+        {
-+                add_calc(id,'.');
-+        }
-+        else if(evt.keyCode==190)
-+        {
-+                add_calc(id,'.');
-+        }
-+        else if(evt.keyCode==188)
-+        {
-+                add_calc(id,'.');
-+        }
-+        else if(evt.keyCode==13)
-+        {
-+                f_calc(id,'=');
-+        }
-+        else if(evt.keyCode==46)
-+        {
-+                f_calc(id,'ce');
-+        }
-+        else if(evt.keyCode==8)
-+        {
-+                f_calc(id,'nbs');
-+        }
-+        else if(evt.keyCode==27)
-+        {
-+                f_calc(id,'ce');
-+        }
 +        return true;
 +}
